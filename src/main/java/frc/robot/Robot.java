acee95410f0f9feea20a6f5473443a0de3cb5ae2
Nano55555 <nguyen.n@prhsrobotics.com>
2023-01-28 12:49:06 -0500

    k

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,10 +9,12 @@ import java.util.concurrent.DelayQueue;
 
 import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
 
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -33,36 +35,16 @@ public class Robot extends TimedRobot {
   public static final WPI_TalonFX leftfrontmotor = Constants.leftfrontmotor;
   public static final WPI_TalonFX rightbackmotor = Constants.rightbackmotor;
   public static final WPI_TalonFX rightfrontmotor = Constants.rightfrontmotor;
+  public Encoder encoder = new Encoder(0, 1, true,EncodingType.k4X);
 
-  public void resetEncoders() {
-    leftbackmotor.setSelectedSensorPosition(0);
-    rightbackmotor.setSelectedSensorPosition(0);
-    leftfrontmotor.setSelectedSensorPosition(0);
-    rightfrontmotor.setSelectedSensorPosition(0);
-  }
-  public static double getRightBackEncoderPosition(){
-    return rightbackmotor.getSelectedSensorPosition();
-  }
+  private final double kDriveTick2Feet = 1.0 / 128 * 6 * Math.PI / 12;
 
-  public static double distanceTravelledMetter(){
-    double dist = getRightBackEncoderPosition() *Meters_Per_Ticks;
-    return (dist);
-  }
-  private static final double In_To_M=.0254;
-  private static final int Motor_Encoder_Codes_Per_Rev=2048;
-  private static final double Diameter_Inches=5.0;
-  private static final double Wheel_Diameter= Diameter_Inches * In_To_M;
-  private static final double Wheel_Circumference= Wheel_Diameter * Math.PI;
-  private static final double Gear_Ratio=12.75;
-  private static final double Ticks_Per_Meter= ( Motor_Encoder_Codes_Per_Rev * Gear_Ratio)/(Wheel_Circumference);
-  private static final double Meters_Per_Ticks= 1/Ticks_Per_Meter;
-
-  public static double kP = 1;
+  public static double kP =1;
   public static double kI =0;
   public static double kD =0;
   public static double lastimestamp = Timer.getFPGATimestamp();
   public static double dt = Timer.getFPGATimestamp() - lastimestamp;
-  public static double setpoint= 2; //meter
+  public static double setpoint= 5; //meter
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -88,7 +70,7 @@ public class Robot extends TimedRobot {
     // commands, running already-scheduled commands, removing finished or interrupted commands,
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
-    SmartDashboard.putNumber("currentpos", distanceTravelledMetter());
+
     CommandScheduler.getInstance().run();
   }
 
@@ -113,7 +95,7 @@ public class Robot extends TimedRobot {
     rightbackmotor.setInverted(true);
     leftfrontmotor.setInverted(false);
 
-    resetEncoders();
+    encoder.reset();
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
@@ -126,7 +108,7 @@ public class Robot extends TimedRobot {
   // @Override
   public void autonomousPeriodic() {
 
-  double currrentpos = distanceTravelledMetter();
+  double currrentpos = encoder.get() * kDriveTick2Feet;
   double error = setpoint - currrentpos;
   double errorsum = dt * error;
   double lasterror = error;
